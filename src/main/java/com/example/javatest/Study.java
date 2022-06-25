08f772780fa4e2b4482695edb2b511c35f9ff6e4
anjonggil <qws458@naver.com>
2022-06-25 14:06:11 +0900

    java test code 스터디
    -반복 테스트

--- src/main/java/com/example/javatest/Study.java
+++ src/main/java/com/example/javatest/Study.java
@@ -1,4 +1,42 @@
 package com.example.javatest;
 
+
 public class Study {
+
+    private StudyStatus studyStatus;
+    private int limit;
+
+    private String name;
+
+    public StudyStatus getStudyStatus() {
+        return studyStatus;
+    }
+
+    public Study() {
+        this.studyStatus = StudyStatus.DRAFT;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
+    public Study(int limit) {
+        if (limit < 0) throw new IllegalArgumentException("limit은 0 보다 커야합니다.");
+        this.limit = limit;
+    }
+
+    public Study(StudyStatus studyStatus, int limit) {
+        this.studyStatus = studyStatus;
+        this.limit = limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "studyStatus=" + studyStatus +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -1,6 +1,10 @@
 package com.example.javatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,9 +14,29 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기") // 이거 사용하자
     void create(){
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
+        Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));
         Study study = new Study();
         assertNotNull(study);
+        assertEquals(StudyStatus.DRAFT,study.getStudyStatus(), () -> "스터디가 생성되면 처음상태는 " +StudyStatus.DRAFT+ " 여야만한다");
+
         System.out.println("create");
+    }
+
+    @RepeatedTest(value = 10, name = "{displayedName}")
+    @DisplayName("반복 테스트 학습")
+    void repeat(RepetitionInfo repetitionInfo){
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @ParameterizedTest
+//    @ValueSource(strings = {"날씨가","많이","더워지고","있네요"})
+    @CsvSource({"10,'자바 스터디'","20,스프링"})
+    void repeatTest(Integer limit,String name){
+        Study study = new Study(limit,name);
+        System.out.println(study);
     }
 
     @Test
